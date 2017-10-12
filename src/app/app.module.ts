d4aaa4c14775c43c94b0f22ed3bd3f65761370ee
Adam Weeks <aoweeks@gmail.com>
2017-10-12 13:40:16 +0000

    Enable routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     NgbModule.forRoot(),
     BrowserModule,
     BrowserAnimationsModule
