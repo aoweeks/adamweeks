c9fcb33e83f39daca71de0eec38eff637adc5300
Adam Weeks <aoweeks@gmail.com>
2017-10-13 00:13:34 +0000

    Make constants for slide left/right transitions

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,7 +1,6 @@
 import {trigger, animate, style, group, query, transition} from '@angular/animations';
 
-export const routerTransition = trigger('routerTransition', [
-  transition('* => about', [
+const slideRight = [
     query(':enter, :leave', style({ position: 'fixed', width:'100%' })
       , { optional: true }),
     group([
@@ -14,8 +13,9 @@ export const routerTransition = trigger('routerTransition', [
         animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
       ], { optional: true }),
     ])
-  ]),
-  transition('about => *', [
+  ];
+  
+const slideLeft = [
     query(':enter, :leave', style({ position: 'fixed', width:'100%' })
       , { optional: true }),
     group([
@@ -28,19 +28,10 @@ export const routerTransition = trigger('routerTransition', [
         animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
       ], { optional: true }),
     ])
-  ]),
-  transition('* <=> *', [
-    query(':enter, :leave', style({ position: 'fixed', width:'100%' })
-      , { optional: true }),
-    group([
-      query(':enter', [
-        style({ transform: 'translateX(0%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(100%)' }))
-      ], { optional: true }),
-      query(':leave', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
-      ], { optional: true }),
-    ])
-  ])
+  ];
+  
+export const routerTransition = trigger('routerTransition', [
+  transition('* => about', slideRight),
+  transition('about => *', slideLeft),
+  transition('* <=> *', slideLeft)
 ])
