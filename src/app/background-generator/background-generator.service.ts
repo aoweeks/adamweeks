b19b1fb10f9069ebaf188a93da34f7d46daa31f6
Adam Weeks <aoweeks@gmail.com>
2017-10-14 16:55:32 +0000

    Implement background pattern

--- src/app/background-generator/background-generator.service.ts
+++ src/app/background-generator/background-generator.service.ts
@@ -3,18 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class BackgroundGeneratorService {
   
-  bgColours: string[] = ['red'];
+  colours: string[] = ['red'];
+  xOffset: number = 0;
+  yOffset: number = 0;
   
   constructor() {
     
   }
   
   public colourChange(baseColour: string) : void{
-      this.bgColours[0] = baseColour;
+    this.colours[0] = baseColour;
   }
 
 
   public toggleColour(){
-  
+    this.xOffset += window.innerWidth;
+    this.yOffset += 10;
   }
 }
