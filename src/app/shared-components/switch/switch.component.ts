eb77c646668f6e4a0691a5812e65b4744493da43
Adam Weeks <aoweeks@gmail.com>
2017-10-25 12:50:25 +0000

    Style functional switch SVG

--- src/app/shared-components/switch/switch.component.ts
+++ src/app/shared-components/switch/switch.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-switch',
+  selector: 'aw-switch',
   templateUrl: './switch.component.html',
   styleUrls: ['./switch.component.scss']
 })
